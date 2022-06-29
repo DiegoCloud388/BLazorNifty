c934dee9dcfa4f824ecbc70ed29368e4ebb5a682
lvlcek <80780963+lvlcek@users.noreply.github.com>
2022-06-29 15:10:24 +0200

    init

---
+++ src/Components/Layout/PageShell.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorNifty.Components.Layout
+{
+    public partial class PageShell
+    {
+        [Parameter] public bool Overlapping { get; set; }
+
+        [Parameter] public bool Rounded { get; set; }
+
+        [Parameter] public string? Class { get; set; }
+
+        [Parameter] public string? Style { get; set; }
+
+        [Parameter] public RenderFragment? ShellHeader { get; set; }
+
+        [Parameter] public RenderFragment? ShellBody { get; set; }
+
+        [Parameter] public RenderFragment? ShellFooter { get; set; }
+
+        private string shellClass = "shell";
+
+        protected override void OnParametersSet()
+        {
+            shellClass = "shell";
+
+            if (Overlapping)
+            {
+                shellClass += " overlaping";
+            }
+
+            if (Rounded)
+            {
+                shellClass += " rounded";
+            }
+
+            if (!string.IsNullOrWhiteSpace(Class))
+            {
+                shellClass += $" {Class}";
+            }
+
+            base.OnParametersSet();
+        }
+    }
+}
