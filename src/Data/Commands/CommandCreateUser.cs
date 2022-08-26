1b1c363f2937a1bc932ace574ab907ab493db8fb
Lukáš Středula <lukas.stredula@outlook.cz>
2022-08-26 14:42:20 +0200

    Příprava na example s validacemi

---
+++ src/Data/Commands/CommandCreateUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorNifty.Data.Commands
+{
+    public class CommandCreateUser
+    {
+        [Required]
+        public string FirtName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public int? StateId { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [Required]
+        public bool? IsGdprConfirmed { get; set; }
+    }
+}
---
+++ src/Pages/Components/Forms/General.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorNifty.Data.Commands;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorNifty.Pages.Components.Forms
+{
+    public partial class General
+    {
+        private CommandCreateUser commandCreate = new CommandCreateUser();
+
+        private async Task OnValidSubmit(EditContext context)
+        {
+            //return null;
+        }
+    }
+}
