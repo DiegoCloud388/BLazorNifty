cb066551aed583f272fd75cb86ed5a9ed7358e93
Lukáš Vítek <diego.vitek6@gmail.com>
2022-12-29 15:35:24 +0100

    Základní funkce wizardu

---
+++ src/Components/Wizards/WizardStep.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorNifty.Components.Wizards
+{
+    public partial class WizardStep<TModel>
+    {
+        /// <summary>
+        /// The <see cref="Wizard"/> container
+        /// </summary>
+        [CascadingParameter]
+        protected internal Wizard<TModel> Parent { get; set; }
+
+        /// <summary>
+        /// The Child Content of the current <see cref="WizardStep"/>
+        /// </summary>
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Parent.AddStep(this);
+        }
+
+        public bool IsLastStep()
+        {
+            return Parent.Steps.LastOrDefault() == this;
+        }
+
+        public bool IsFirstStep()
+        {
+            return Parent.Steps.FirstOrDefault() == this;
+        }
+
+        public bool IsActiveStep()
+        {
+            return Parent.ActiveStep == this;
+        }
+
+    }
+}
