a24652604146ee79338b6df02aeab5761c237201
Lukáš Vítek <lukas.vitek388@outlook.cz>
2023-02-04 08:38:35 +0100

    Wizard style

---
+++ src/Pages/Components/Forms/Wizard/AddressStepWizardModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorNifty.Pages.Components.Forms.Wizard
+{
+    public class AddressStepWizardModel
+    {
+        public string Address { get; set; } = string.Empty;
+
+        public string? Address2 { get; set; }
+
+        public string City { get; set; } = string.Empty;
+
+        public string? ZipCode { get; set; }
+    }
+}
--- src/Pages/Components/Forms/Wizard/TestWizardModel.cs
+++ src/Pages/Components/Forms/Wizard/TestWizardModel.cs
@@ -6,27 +6,11 @@ namespace BlazorNifty.Pages.Components.Forms.Wizard
     {
         public string UserName { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Email je povinné pole")]
         public string Email { get; set; } = string.Empty;
 
         public string Password { get; set; } = string.Empty;
 
-        public string RetypePassword { get; set; } = string.Empty;
-
-        //public string FirstName { get; set; } = string.Empty;
-
-        //public string LastName { get; set; } = string.Empty;
-
-        //public string? Company { get; set; }
-
-        //public string? MemberType { get; set; }
-
-        //public string Address { get; set; } = string.Empty;
-
-        //public string? Address2 { get; set; }
-
-        //public string City { get; set; } = string.Empty;
-
-        //public string? ZipCode { get; set; }
+        public string RetypePassword { get; set; } = string.Empty;  
     }
 }
